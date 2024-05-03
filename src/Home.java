73b21d6c9f4f0ac9e26156f211b221d7b09d078b
Anže Krobot <anze.krobot@scv.si>
2024-05-03 16:21:13 +0200

    dodana osnova

---
+++ src/Home.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Home {
+
+    private JFrame window;
+    private Container container;
+
+    private JLabel mainTitle;
+
+    public Home() {
+        window = new JFrame("Lanparty aplikacija"); // Ustvarimo novo okno
+        window.setPreferredSize(new Dimension(1024, 768)); // Nastavimo velikost okna
+        window.setBounds(10, 10, 1024, 768); // Nastavimo pozicijo in velikost okna
+        window.setLayout(null); // Nastavimo postavitev okna
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Nastavimo akcijo ob zaprtju okna
+        window.setLocationRelativeTo(null); // Nastavimo pozicijo okna na sredino
+        window.setResizable(false); // Omogočimo spreminjanje velikosti okna
+
+        container = window.getContentPane(); // Ustvarimo nov container
+
+        mainTitle = new JLabel("Nadzorna plošča"); // Ustvarimo nov label
+        mainTitle.setFont(new Font("Arial", Font.BOLD, 48)); // Nastavimo velikost in obliko pisave
+        mainTitle.setBounds(10, 50, 1004, 50); // Nastavimo pozicijo in velikost
+        container.add(mainTitle); // Dodamo label v container
+    }
+
+    public void show() {
+        window.setVisible(true);
+    }
+}
