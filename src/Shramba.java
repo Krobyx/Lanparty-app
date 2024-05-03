bc46fa1f07af1fe58e32c1d850f73c0d3107ca83
Anže Krobot <anze.krobot@scv.si>
2024-05-03 17:37:09 +0200

    dodana shramba

---
+++ src/Shramba.java
@@ -0,0 +1,21 @@
+public class Shramba {
+    // Singleton vzorec
+    private static Shramba instance;
+    public int uporabnikId;
+
+    private Shramba() {
+
+    }
+
+    // Singleton metoda za dostop do instance
+    public static synchronized Shramba getInstance() {
+        if (instance == null) {
+            instance = new Shramba();
+        }
+        return instance;
+    }
+
+    public boolean isAdministrator() {
+        return this.uporabnikId == 1;
+    }
+}
